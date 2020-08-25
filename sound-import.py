0f457c5b3970114e9cee0a6e0a6f9f93f7db01c2
Christopher Toth <q.alpha@gmail.com>
2020-08-24 22:44:12 -0600

    Modified to work with multiple input files

--- sound-import.py
+++ sound-import.py
@@ -8,9 +8,10 @@ class Sound(object):
   self.filename = os.path.splitext(os.path.basename(in_file))[0]
   self.out_file = os.path.join(out_dir, self.filename + ".m4a")
   self.out_dir = out_dir
-  self.temp_wav = os.path.join(out_dir, self.filename + ".wav")
+  self.output_wav = os.path.join(out_dir, self.filename + ".wav")
   self.metadata = ffmpeg.probe(in_file)
-  self.temp_filename = ""
+  self._intermediate_filename = ""
+  self._intermediate_fd = 0
 
  @classmethod
  def new_sound(cls, *args):
@@ -25,61 +26,54 @@ class Sound(object):
   return 0
 
  def __dell__(self):
-  if self.temp_filename:
-   os.close(self.temp_fd)
-   os.remove(self.temp_filename)
-   os.remove(self.temp_wav)
+  if self.intermediate_filename:
+   os.close(self._intermediate_fd)
+   os.remove(self.intermediate_filename)
+   os.remove(self.output_wav)
 
  @property
- def get_temp_filename(self):
-  if not self.temp_filename:
-   (self.temp_fd, self.temp_filename) = tempfile.mkstemp()
-  return self.temp_filename
+ def intermediate_filename(self):
+  if not self._intermediate_filename:
+   self.intermediate_fd, self._intermediate_filename = tempfile.mkstemp()
+  return self._intermediate_filename
 
- def convert(self):
-  ffmpeg.input(self.in_file).audio.output(self.get_temp_filename, format="wav").overwrite_output().run()
-  self.process(self.get_temp_filename)
-  ffmpeg.input(self.temp_wav, format="wav").audio.output(self.out_file, format="adts").overwrite_output().run()
-  os.close(self.temp_fd)
-  os.remove(self.get_temp_filename)
-  os.remove(self.temp_wav)
-
- def process(self, in_file):
-  trf = sox.Transformer()
-  trf.silence(location=1, buffer_around_silence=True)
-  trf.reverse()
-  trf.silence(location=1, buffer_around_silence=True)
-  trf.reverse()
-  trf.norm()
-  trf.set_input_format(file_type="wav")
-  trf.build(in_file, self.temp_wav)
+ def convert_to_intermediate(self):
+  ffmpeg.input(self.in_file).audio.output(self.intermediate_filename, format="wav").overwrite_output().run()
+ 
+ def process_intermediate(self):
+  run_import_chain(self.intermediate_filename, self.output_wav)
+  
+ def render_output(self):
+  ffmpeg.input(self.intermediate_filename, format="wav").audio.output(self.out_file, vbr=5, format="adts", ).overwrite_output().run()
+  os.close(self.intermediate_fd)
+  os.remove(self.intermediate_filename)
+  os.remove(self.output_wav)
 
 def convert_sound_file(in_file, out_dir):
+ if not os.path.exists(out_dir):
+  os.makedirs(out_dir)
+ print(in_file)
  sound = Sound.new_sound(in_file, out_dir)
- if sound:
-  sound.convert()
-  del sound
+ if not sound:
+   return
+ sound.convert_to_intermediate()
+ sound.process_intermediate()
+ sound.render_output()
 
-def run(*args):
- global in_file
- global out_dir
- check_args(*args)
- convert_sound_file(in_file, out_dir)
+def run_import_chain(in_file, out_file):
+ trf = sox.Transformer()
+ trf.silence(location=1, buffer_around_silence=True)
+ trf.reverse()
+ trf.silence(location=1, buffer_around_silence=True)
+ trf.reverse()
+ trf.norm()
+ trf.set_input_format(file_type="wav")
+ trf.build(in_file, out_file)
 
-def check_args(*args):
- global in_file
- global out_dir
- try:
-  in_file = args[1]
-  out_dir = args[2]
- except IndexError:
-  raise SystemExit(f"Usage: {sys.argv[0]} <in_file> <out_dir>")
- if not os.path.isfile(in_file):
-  raise SystemExit(f"Invalid input file {in_file=}")
- if not os.path.isdir(out_dir):
-  raise SystemExit(f"Invalid output directory {out_dir=}")
- in_file = os.path.abspath(in_file)
- out_dir = os.path.abspath(out_dir)
+def main():
+ out_dir = sys.argv[1]
+ files = sys.argv[2:]
+ list(map(lambda f: convert_sound_file(f, out_dir), files))
 
-if __name__ == "__main__":
- run(*sys.argv)
+if __name__ == '__main__':
+ main()
